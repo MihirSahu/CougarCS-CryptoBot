548c080e8f8978bcccc2ca68638d696f6e3111a9
Mihir Sahu <2002mihir@gmail.com>
2022-04-11 20:39:32 -0500

    Added env

---
+++ api/settings.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+#MONGO_URI = os.getenv('MONGO_URI')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+#CONFIG_ID = os.getenv('CONFIG_OBJECT_ID')
+#HOST = os.getenv('HOST')
